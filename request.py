1325d7339d8d62795c88b1f244878f67fd67e446
MahmoudAbd0 <Mahmoud--Abdo@outlook.com>
2025-09-06 04:30:01 +0300

    feat: Add Request class

--- request.py
+++ request.py
@@ -6,16 +6,74 @@ class Request:
         self.query_params = {}
         self.http_version = None
         self.headers = {}
-        self.body = {}
+        self.body = ""
 
         self.parse_request()
 
-        def parse_request(self):
-            splitted_data = self.data.split("\r\n\r\n",1)
-            request_head = splitted_data[0]
-            self.body = splitted_data[1] if len(splitted_data) > 1 else ""
+    def parse_request(self):
+        splitted_data = self.data.split("\r\n\r\n",1)
+        request_head = splitted_data[0]
+        request_body = splitted_data[1] if len(splitted_data) > 1 else ""
+        self.body = request_body
 
-            pass
+        head_lines = request_head.splitlines()
+
+        start_line = head_lines[0]
+        self._parse_start_line(start_line)
+        self._parse_head_lines(head_lines)
+        
+     
+    
+    def get_header(self, key, default = None):
+        return self.headers.get(key, default)
+    
+
+    def json(self):
+        if self.get_header("content-type") == "application/json":
+            import json
+            return json.loads(self.body)
+        return None
+
+    def _parse_start_line(self, start_line):
+        splitted_start_line = start_line.split()
+        try:
+            self.method = splitted_start_line[0]
+            self.http_version = splitted_start_line[2]
+        except IndexError:
+            raise IndexError('Invalid request start line format')
+        
+        path_and_params = splitted_start_line[1]
+        splitted_path_and_params = path_and_params.split("?")
+        self.path = splitted_path_and_params[0]
+
+        if len(splitted_path_and_params) > 1:
+            params = splitted_path_and_params[1].split("&")
+
+            for param in params:
+                param_key, _, param_value = param.partition('=')
+                self.query_params[param_key] = param_value or True
+
+    def _parse_head_lines(self, head_lines):
+
+        if len(head_lines) > 1:
+            for header in head_lines[1:]:
+                if not header:
+                    continue
+                try:
+                        header_key, header_value = header.split(": ",1)
+                        self.headers[header_key.lower().strip()] = header_value
+                except ValueError:
+                    pass
+
+
+    def __repr__(self):
+        return f"<Request \n METHOD: {self.method} \n PATH: {self.path} \n HTTP_VERSION: {self.http_version} \n QUERY PARAMS: {self.query_params} \n HEADERS: {self.headers} \n BODY: {self.body}>"
+
+
+
+data = """POST /users?id=42&flag HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent:curl/7.68.0\r\nContent-Type: application/json\r\nContent-Length: 18\r\n\r\n{"name": "Ali"}"""
+request = Request(data)
+print(request)
 
 
 
