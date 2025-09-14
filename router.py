5faf5178a4ed49a5a5ebcb97ae6ef6a823cb1670
MahmoudAbd0 <Mahmoud--Abdo@outlook.com>
2025-09-14 15:42:37 +0300

    feat(Router): add helper methods for common HTTP methods

--- router.py
+++ router.py
@@ -10,6 +10,19 @@ class Router:
 
         self.routes[method][path] = handler
 
+    def get(self, path, handler):
+        return self.add_route(path, "GET", handler )
+
+    def post(self, path, handler):
+        return self.add_route(path, "POST", handler )
+
+    def put(self, path, handler):
+        return self.add_route(path, "PUT", handler )
+
+    def delete(self, path, handler):
+        return self.add_route(path, "DELETE", handler )
+
+
     def resolve(self, request):
         method_routes = self.routes.get(request.method, {})
         handler = method_routes.get(request.path)
