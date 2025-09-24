a3edc343e9ff932bec9a1011f02088930026e1b4
MahmoudAbd0 <Mahmoud--Abdo@outlook.com>
2025-09-24 23:22:59 +0300

    update(Response): extend Response class to send cookie headers

--- server/response.py
+++ server/response.py
@@ -56,6 +56,7 @@ class Response:
         self.status = status
         self.headers = {k.lower(): v for k,v in (headers or {}).items()}
         self.body = body if isinstance(body, (str, bytes)) else str(body)
+        self.cookies = []
 
         # Ensure a default content-type if not provided
         if "content-type" not in self.headers:
@@ -81,16 +82,57 @@ class Response:
        body_in_bytes = self.body.encode("utf-8") if isinstance(self.body, str) else self.body
        body_length = len(body_in_bytes)
 
-       status_reason = self.STATUS_MESSAGES.get(self.status, "")
+       status_reason = self.STATUS_MESSAGES.get(self.status, "Unknown")
        status_line = f"HTTP/1.1 {self.status} {status_reason}\r\n"
 
        self.headers["content-length"] = str(body_length)
 
-       headers_in_str = "".join( f"{self._format_header_name(key)}: {value}\r\n" for key, value in self.headers.items())
+       headers_in_str = "".join( f"{self._format_header_name(key)}: {value}\r\n" for key, value in self.headers.items() if key != "set-cookie")
+       cookie_headers = "".join(f"Set-Cookie: {cookie}\r\n" for cookie in self.cookies)
 
-       return (status_line + headers_in_str + "\r\n").encode("utf-8") + body_in_bytes
 
+       return (status_line + headers_in_str + cookie_headers + "\r\n").encode("utf-8") + body_in_bytes
+
+
+    def set_cookie(
+        self,
+        name ,
+        value ,
+        path  = "/",
+        domain  = None,
+        max_age  = None,
+        expires  = None,
+        secure  = False,
+        http_only = False,
+        same_site  = None,
+        ):
+        cookie_parts = [f"{name}={value}"]
+
+        if same_site:
+            same_site = same_site.capitalize()
+        if same_site not in ("Strict", "Lax", "None"):
+            raise ValueError("Invalid SameSite value: must be Strict, Lax, or None")
+
+        attributes = {
+        "Path": (path, False),
+        "Domain": (domain, False),
+        "Max-Age": (max_age, False),
+        "Expires": (expires, False),
+        "Secure": (secure, True),
+        "HttpOnly": (http_only, True),
+        "SameSite": (same_site, False),
+        } 
+
+
+        for key, (value, is_flag) in attributes.items():
+            if is_flag and value:
+                cookie_parts.append(key)
+            elif value is not None:
+                cookie_parts.append(f"{key}={value}")
+
+        self.cookies.append("; ".join(cookie_parts))
 
     def __repr__(self):
-        return f"<Response {self.status} {self.STATUS_MESSAGES.get(self.status, '')}, Headers={self.headers}, Body={self.body!r}>"
+        return f"<Response {self.status} {self.STATUS_MESSAGES.get(self.status, '')}, Headers={self.headers}, Cookies = {self.cookies}, Body={self.body!r}>"
+
 
